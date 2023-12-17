bb8efbcf93a5d83c73fa2be7e4880cb3e602b6e0
Mina Nabil <minanabil2004@gmail.com>
2023-12-17 11:11:07 +0200

    Add Colors

--- Game.c
+++ Game.c
@@ -11,20 +11,28 @@
 #define rows_c 2
 #define cols_c 4
 
-void check_box(int rows,int cols, char A[rows][cols],int r1,int r2,int c1,int c2){
+void check_box(int rows,int cols, char A[rows][cols],int r1,int r2,int c1,int c2,int F[rows][cols]){
     if(r1==r2){
         int c = c1<c2 ? c1:c2;
-        if(A[r1-1][c1]!=' '&&A[r1-1][c2]!=' '&&A[r1-rows_c][c+1]!=' ')
+        if(A[r1-1][c1]!=' '&&A[r1-1][c2]!=' '&&A[r1-rows_c][c+1]!=' '){
             A[r1-1][c+1]=A[r1-1][c+2]=A[r1-1][c+3]=178;
-        if(A[r1+1][c1]!=' '&&A[r1+1][c2]!=' '&&A[r1+rows_c][c+1]!=' ')
+            F[r1-1][c+1]=F[r1-1][c+2]=F[r1-1][c+3]=1;
+        }
+        if(A[r1+1][c1]!=' '&&A[r1+1][c2]!=' '&&A[r1+rows_c][c+1]!=' '){
             A[r1+1][c+1]=A[r1+1][c+2]=A[r1+1][c+3]=178;
+            F[r1+1][c+1]=F[r1+1][c+2]=F[r1+1][c+3]=1;
+        }
     }
     else if(c1==c2){
         int r = r1<r2 ? r1:r2;
-        if(A[r1][c1-1]!=' '&&A[r2][c1-1]!=' '&&A[r+1][c1-cols_c]!=' ')
+        if(A[r1][c1-1]!=' '&&A[r2][c1-1]!=' '&&A[r+1][c1-cols_c]!=' '){
             A[r+1][c1-1]=A[r+1][c1-2]=A[r+1][c1-3]=178;
-        if(A[r1][c1+1]!=' '&&A[r2][c1+1]!=' '&&A[r+1][c1+cols_c]!=' ')
+            F[r+1][c1-1]=F[r+1][c1-2]=F[r+1][c1-3]=1;
+        }
+        if(A[r1][c1+1]!=' '&&A[r2][c1+1]!=' '&&A[r+1][c1+cols_c]!=' '){
             A[r+1][c1+1]=A[r+1][c1+2]=A[r+1][c1+3]=178;
+            F[r+1][c1+1]=F[r+1][c1+2]=F[r+1][c1+3]=1;
+        }
     }
 }
 
@@ -42,19 +50,36 @@ void create_grid(int  rows,int cols,char A[rows][cols]){
         }
 }
 
-void move(int rows,int cols, char A[rows][cols],int r1,int r2,int c1,int c2){
+void move(int rows,int cols, char A[rows][cols],int r1,int r2,int c1,int c2,int F[rows][cols]){
     if(r1==r2){
         int c = c1<c2 ? c1:c2;
         A[r1][c+1]=A[r1][c+2]=A[r1][c+3]=205;
+        F[r1][c+1]=F[r1][c+2]=F[r1][c+3]=1;
         }
     else if(c1==c2){
         int r = r1<r2?r1:r2;
         A[r1+1][c1]=186;
+        F[r1+1][c1]=1;
         }
-    check_box(rows,cols,A,r1,r2,c1,c2);
+    check_box(rows,cols,A,r1,r2,c1,c2,F);
 }
 
-void print(char *arr, int m, int n)
+void print(int rows,int cols,char A[rows][cols],int R[rows][cols],int B[rows][cols]){
+    for (int i=0;i<rows;i++){
+        printf("\n");
+      for (int j=0;j<cols;j++){
+        if(R[i][j]==1)
+            printf(RED"%c"RESET,A[i][j]);
+        else if(B[i][j]==1)
+            printf(BLU"%c"RESET,A[i][j]);
+        else
+            printf("%c",A[i][j]);
+      }
+    }
+    printf("\n");
+}
+
+/*void print(char *arr, int m, int n)
 {
     int i, j;
     for (i = 0; i < m; i++){
@@ -63,7 +88,7 @@ void print(char *arr, int m, int n)
         printf("%c", *((arr+i*n) + j));
     }
     printf("\n");
-}
+}*/
 
 int main() {
     int n,m;
@@ -73,8 +98,13 @@ int main() {
     int cols = cols_c*m+1;
 
     char A[rows][cols];
+    int R[rows][cols],B[rows][cols];
+    for(int i=0;i<rows;i++)
+        for(int j=0;j<cols;j++)
+            R[i][j]=B[i][j]=0;
     create_grid(rows,cols,A);
-    print((char *)A, rows, cols);
+    print(rows,cols,A,R,B);
+    /*print((char *)A, rows, cols);*/
 
     int r1,r2,c1,c2;
     for(int k=0;k<4;k++){
@@ -83,10 +113,19 @@ int main() {
     r2=rows_c*r2;
     c1=cols_c*c1;
     c2=cols_c*c2;
-    move(rows,cols,A,r1,r2,c1,c2);
-    system("cls");
-    print((char *)A, rows, cols);
+        if(k%2==0){
+            move(rows,cols,A,r1,r2,c1,c2,R);
+            system("cls");
+            print(rows,cols,A,R,B);
+            /*print((char *)A, rows, cols);*/
+        }
+        else{
+            move(rows,cols,A,r1,r2,c1,c2,B);
+            system("cls");
+            print(rows,cols,A,R,B);
+            /*print((char *)A, rows, cols);*/
+        }
     }
-    system("cls");
-    print((char *)A, rows, cols);
+    /*system("cls");*/
+    /*print((char *)A, rows, cols);*/
 }
