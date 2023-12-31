b94884d5cbc5f741ed32c5d110aea5a40e40e465
Mina Nabil <minanabil2004@gmail.com>
2023-12-31 19:46:56 +0200

    Adjust the menus restrictions|make menus work with switshs|add ending menu

--- Menus.h
+++ Menus.h
@@ -29,91 +29,92 @@ player topPlayers[MAX_PLAYERS];
 FILE *file;
 
 
-
 void grid_menu(int *pn,int *pm){
-system("cls");
-printf("Select Grid Size\n");
-printf( CYN"1- Easy | 2x2 Grid\n\n"RESET);
-printf( RED"2- Expert | 5x5 Grid\n\n"RESET);
-printf( YEL"3- Custom Grid\n\n"RESET);
-int input;
-scanf("%d",&input);
-    if (input == 1){*pn=2;*pm=2;}
-    else if (input == 2){ *pn=5;*pm=5;}
-    else if (input == 3){ printf("Enter your grid n*n: ");scanf("%d*%d",pn,pm);}
-    else {printf("Please enter a valid choice\n");
-    sleep(1);
-    system("cls");
-    grid_menu(pn,pm);}
+    system("cls");              //printing choices
+    printf("Select Grid Size\n");
+    printf( CYN"1- Easy | 2x2 Grid\n\n"RESET);
+    printf( RED"2- Expert | 5x5 Grid\n\n"RESET);
+    printf( YEL"3- Custom Grid\n\n"RESET);
+
+    int input;
+    do{
+    printf( "Select A Number: ");       //geting valid input
+        if(scanf("%d",&input)!=1);
+            while(getchar()!='\n');
+    }while(input!=1&&input!=2&&input!=3);
+    switch(input){
+    case 1:*pn=2;*pm=2;break;   //Easy | 2x2 Grid
+    case 2:*pn=5;*pm=5;break;   //Expert | 5x5 Grid
+    case 3:printf("Enter your grid n*n: ");scanf("%d*%d",pn,pm);break;  //Custom Grid
     }
+}
 
 
 void mode_menu(int *pn,int *pm,int *pmode,player *p1,player *p2){
     system("cls");
-    printf("Select Game Mode\n");
+    printf("Select Game Mode\n");               //printing choices
     printf( YEL"1- Human vs. Human\n\n"RESET);
     printf( CYN"2- Human vs. Computer\n\n"RESET);
+    
     int input;
-    scanf("%d",&input);
-    if (input == 1){
+    do{
+    printf( "Select A Number: ");       //geting valid input
+        if(scanf("%d",&input)!=1);
+            while(getchar()!='\n');
+    }while(input!=1&&input!=2);
+
+    switch(input){
+    case 1:                     //Human vs. Human
         *pmode = 0;
-        printf("Enter Player 1 name: ");    scanf( "%c", (char *) stdin);   gets(p1->name);
-        printf("Enter Player 2 name: ");   gets(p2->name);
-        grid_menu(pn,pm);}
-    else if (input == 2){
+        printf("Enter Player 1 name: ");scanf( "%c", (char *) stdin);gets(p1->name); //get first player name
+        printf("Enter Player 2 name: ");gets(p2->name);     //get second player name
+        grid_menu(pn,pm);
+        break;
+    case 2:                     //Human vs. Computer
         *pmode = 1;
-       printf("Enter Player name: ");scanf( "%c", (char *) stdin);   gets(p1->name);
+       printf("Enter Player name: ");scanf( "%c", (char *) stdin);gets(p1->name);  //get player name
        strcpy(p2->name, "Mina&John");
-       grid_menu(pn,pm);}
-    else {printf("Please enter a valid choice\n");
-    sleep(1);
-    system("cls");
-    mode_menu(pn,pm,pmode,p1,p2);}
+       grid_menu(pn,pm);
+       break;
     }
-
-
-
-
+}
 
 
 void main_menu(int *pn,int *pm, int *pmode, player *p1, player *p2){
+    printf( CYN"1- New Game\n\n"RESET);         //printing the choices
+    printf( BLU"2- Load Game\n\n"RESET);
+    printf( YEL"3- Top 10 Players\n\n"RESET);
+    printf( GRN"4- EXIT\n\n"RESET);
 
-printf( CYN"1- New Game\n\n"RESET);
-printf( BLU"2- Load Game\n\n"RESET);
-printf( YEL"3- Top 10 Players\n\n"RESET);
-printf( GRN"4- EXIT\n\n"RESET);
-printf( "Select A Number: ");
-int input;
-scanf("%d",&input);
-    if(input == 1){
-        mode_menu(pn,pm,pmode,p1,p2);
-}
-    else if (input == 2){loadGame("lol.txt",p1, p2);}
+    int input;
+    do{
+        printf( "Select A Number: ");       //geting valid input
+        if(scanf("%d",&input)!=1);
+            while(getchar()!='\n');
+    }while(input!=1&&input!=2&&input!=3&&input!=4);
 
-    else if (input == 3){
-    printf("\n");
-    display();
-    sleep(5);
-    system("cls");
-    main_menu(pn,pm,pmode,p1,p2);
+    switch(input){
+        case 1:mode_menu(pn,pm,pmode,p1,p2);break;  //New Game
+        case 2:loadGame("lol.txt",p1, p2);break;    //Load Game
+        case 3:printf("\n");display();sleep(5);system("cls");main_menu(pn,pm,pmode,p1,p2);break;   //Top 10 Players
+        case 4:exit(0);     //Exit
     }
-
-    else if (input == 4){ exit(0);}
-
-    else    {printf("Please enter a valid choice\n");
-    sleep(1);
-    system("cls");
-    main_menu(pn,pm,pmode,p1,p2);}
 }
 
 
+void ending_menu(int *pn,int *pm, int *pmode, player *p1, player *p2){
+    printf( CYN"1- Main Menu\n\n"RESET);         //printing the choices
+    printf( BLU"2- Exit\n\n"RESET);
 
+    int input;
+    do{
+        printf( "Select A Number: ");       //geting valid input
+        if(scanf("%d",&input)!=1);
+            while(getchar()!='\n');
+    }while(input!=1&&input!=2);
 
-
-
-
-
-
-
-
-
+    switch(input){
+        case 1:system("cls");main_menu(pn,pm,pmode,p1,p2);break;  //Main Menu
+        case 2:exit(0);     //Exit
+    }
+}
